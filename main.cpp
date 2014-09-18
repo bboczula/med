c3bd89e976437d19cd66524acdcaea73c4448da4
bboczula <bb.bartosh@gmail.com>
2014-09-18 12:32:03 +0200

    Refactoring, extracted file reading to separate class

--- main.cpp
+++ main.cpp
@@ -1,7 +1,37 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <vector>
 using namespace std;
+
+class CStorage
+{
+	vector<string> storage;
+	int totalNumberOfLines;
+	void readFile(string name)
+	{
+		cout << "Reading file " << name << "..." << endl;
+		ifstream file(name.c_str());
+		string line;
+		while(getline(file, line))
+		{
+			storage.push_back(line);
+			totalNumberOfLines++;
+		}
+		file.close();
+		cout << totalNumberOfLines << " lines read successfully." << endl;
+	}
+public:
+	CStorage(string filename)
+	{
+		totalNumberOfLines = 0;
+		readFile(filename);
+	}
+	string getLineAt(int lineNumber)
+	{
+		return storage[lineNumber];
+	}
+};
 
 string prompt()
 {
@@ -9,14 +39,32 @@ string prompt()
 	return mode + "> ";
 }
 
+
 int main()
 {
-	ifstream file("main.cpp");
+	CStorage storage("main.cpp");
+
+	int lineNumber = 0;
 	string choice;
 	while(true)
 	{
 		cout << prompt();
 		cin >> choice;
+		if(choice == "p")
+		{
+			cout << storage.getLineAt(lineNumber)<< endl;
+			continue;
+		}
+		if(choice == "+")
+		{
+			lineNumber++;
+			continue;
+		}
+		if(choice == "-")
+		{
+			lineNumber--;
+			continue;
+		}
 		if(choice == "q")
 		{
 			return 0;
