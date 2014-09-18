55ffbd10d9d69e6079943f33be3fd199b3086b58
bboczula <bb.bartosh@gmail.com>
2014-09-18 11:26:23 +0200

    Added first version of command-line parsing

--- main.cpp
+++ main.cpp
@@ -1,4 +1,22 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
 int main()
 {
+	ifstream file("main.cpp");
+	string mode = "NORMAL";
+	string prompt = mode + "> ";
+	string line;
+	while(true)
+	{
+		cout << prompt;
+		cin >> line;
+		if(line == "q")
+		{
+			return 0;
+		}
+	}
 	return 0;
 }
