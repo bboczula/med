99ecf087ccbf03867fac58f700cdea78084d774f
bboczula <bb.bartosh@gmail.com>
2014-09-25 13:48:50 +0200

    Some minor tweaking and refactoring to increase compatibility

--- code/CInputHandler.cpp
+++ code/CInputHandler.cpp
@@ -17,43 +17,58 @@ bool CInputHandler::process(string commandString)
 
 ICommand* CInputHandler::getCommand(string commandString)
 {
-    if(commandString == "p")
+    if(commandString.size() == 1)
     {
-        return new CPrintCommand();
+        if(commandString == "p" || commandString == ".")
+        {
+            return new CPrintCommand();
+        }
+        if(commandString == "P")
+        {
+            return new CPrintAroundCommand();
+        }
+        if(commandString == "+")
+        {
+            CCommandComposite* incrementAndPrint = new CCommandComposite();
+            incrementAndPrint->add(new CIncrementLineCommand());
+            incrementAndPrint->add(new CPrintCommand());
+            return incrementAndPrint;
+        }
+        if(commandString == "-")
+        {
+            CCommandComposite* decrementAndPrint = new CCommandComposite();
+            decrementAndPrint->add(new CDecrementLineCommand());
+            decrementAndPrint->add(new CPrintCommand());
+            return decrementAndPrint;
+        }
+        if(commandString == "1")
+        {
+            CCommandComposite* goToFirstLineAndPrint = new CCommandComposite();
+            goToFirstLineAndPrint->add(new CFirstLineCommand());
+            goToFirstLineAndPrint->add(new CPrintCommand());
+            return goToFirstLineAndPrint;;
+        }
+        if(commandString == "$")
+        {
+            CCommandComposite* goToLastLineAndPrint = new CCommandComposite();
+            goToLastLineAndPrint->add(new CLastLineCommand());
+            goToLastLineAndPrint->add(new CPrintCommand());
+            return goToLastLineAndPrint;
+        }
+        if(commandString == "q")
+        {
+            return new CQuitCommand();
+        }
     }
-    if(commandString == "pp")
+    else if(commandString.size() == 2)
     {
-        CCommandComposite* doublePrint = new CCommandComposite();
-        doublePrint->add(new CPrintCommand());
-        doublePrint->add(new CIncrementLineCommand());
-        doublePrint->add(new CPrintCommand());
-        return doublePrint;;
-    }
-    if(commandString == "P")
-    {
-        return new CPrintAroundCommand();
-    }
-    if(commandString == "+")
-    {
-        CCommandComposite* incrementAndPrint = new CCommandComposite();
-        incrementAndPrint->add(new CIncrementLineCommand());
-        incrementAndPrint->add(new CPrintCommand());
-        return incrementAndPrint;
-    }
-    if(commandString == "-")
-    {
-        return new CDecrementLineCommand();
-    }
-    if(commandString == "1")
-    {
-        return new CFirstLineCommand();
-    }
-    if(commandString == "$")
-    {
-        return new CLastLineCommand();
-    }
-    if(commandString == "q")
-    {
-        return new CQuitCommand();
+        if(commandString == "pp")
+        {
+            CCommandComposite* doublePrint = new CCommandComposite();
+            doublePrint->add(new CPrintCommand());
+            doublePrint->add(new CIncrementLineCommand());
+            doublePrint->add(new CPrintCommand());
+            return doublePrint;;
+        }
     }
 }
